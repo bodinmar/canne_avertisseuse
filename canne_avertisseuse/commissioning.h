a19de75551a43155f0da5c4e8e53fe0216f32bb5
hugonad <65532705+hugonad@users.noreply.github.com>
2020-06-10 15:26:09 +0200

    Update commissioning.h

--- canne_avertisseuse/commissioning.h
+++ canne_avertisseuse/commissioning.h
@@ -15,8 +15,8 @@
  */
 /*OTAA connection adresse*/
 /*--------------------------------------------*/
-#define APP_EUI "70B3D57ED001D6F7"
-#define APP_KEY "B06B48A1958A122245887BD1A83D1CA5"
+#define APP_EUI ""
+#define APP_KEY ""
 /*------------------------------------------*/
 /*ABP connection adresss
 --------------------------------------------*/
